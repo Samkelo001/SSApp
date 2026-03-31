f03c07bcc69979c656dcdd3b9ec6d0a2bc98875f
NgemaVC <fngema@varsitycollege.co.za>
2026-03-31 13:45:31 +0200

    First Commit Second Commit

--- app/src/main/java/com/example/starsucksapp/MainActivity.kt
+++ app/src/main/java/com/example/starsucksapp/MainActivity.kt
@@ -1,25 +1,35 @@
 package com.example.starsucksapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.starsucksapp.databinding.ActivityMainBinding
+import com.example.starsucksapp.databinding.ActivityMainWithNavBinding
+import com.example.starsucksapp.databinding.ActivityMainWithNavDrawer2Binding
+import com.example.starsucksapp.databinding.ActivityMainWithNavDrawerBinding
+//import android.view.MenuItem
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+    var order = order()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val binding = ActivityMainWithNavBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.idSb1.setOnClickListener(this)
@@ -29,23 +39,111 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.idSb5.setOnClickListener(this)
         binding.idSb6.setOnClickListener(this)
 
-        /*
-        binding.idSb1.setOnClickListener {
-            Toast.makeText(this, "this is a toast", Toast.LENGTH_SHORT).show()
+        setSupportActionBar(binding.navToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
 
-        }*/
+        var toggleOnOff = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout, binding.navToolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(toggleOnOff)
+        toggleOnOff.syncState()
 
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
+        binding.navView.bringToFront()
+        binding.navView.setNavigationItemSelectedListener(this)
+        
     }
 
     override fun onClick(v : View?) {
         when(v?.id){
-            R.id.id_sb1 -> Toast.makeText(this,"MMM Say Latte",Toast.LENGTH_SHORT).show()
-        }
+            R.id.id_sb1 -> order.productName = "Soy Latte"
+            R.id.id_sb2 -> order.productName = "Chocco Frappe"
+            R.id.id_sb3-> order.productName = "Bottled Americano"
+
+         }
+        openIntent(applicationContext, order.productName, OrderDetailsActivity::class.java)
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_main -> {
+                Toast.makeText(this, "Menu is clicked", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.take_photo -> {
+                Toast.makeText(this, "Take photo is clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
+    }
+}
+class order(){
+    lateinit var productName: String
+    lateinit var customerName: String
+    lateinit var customerCell: String
+    lateinit var orderDate: String
+
+    constructor(pName: String) : this() {
+        productName = pName
+    }
+
+    constructor(pName: String, cName: String, cCell: String,
+                oDate: String) : this(pName) {
+        customerName = cName
+        customerCell = cCell
+        orderDate = oDate
+    }
+
+}
+
+fun openIntent(context: Context, order: String, activityToOpen: Class<*>){
+    val intent = Intent(context, activityToOpen)
+
+    intent.putExtra("order", order)
+
+    if(context !is android.app.Activity){
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    }
+
+    context.startActivity(intent)
+}
+
+fun shareIntent(context: Context, order: String){
+    val sendIntent = Intent()
+
+    sendIntent.action = Intent.ACTION_SEND
+
+    sendIntent.putExtra(Intent.EXTRA_TEXT, order)
+
+    sendIntent.type = "text/plain"
+
+    val shareIntent = Intent.createChooser(sendIntent, null)
+
+    if(context !is android.app.Activity){
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    }
+    context.startActivity(shareIntent)
+}
+
+
+fun shareIntent(context: Context, order: order){
+    val sendIntent = Intent()
+
+    val shareOrderDetails = Bundle()
+    shareOrderDetails.putString("productName", order.productName)
+    shareOrderDetails.putString("customerName", order.customerName)
+    shareOrderDetails.putString("customerCell", order.customerCell)
+
+    sendIntent.putExtra(Intent.EXTRA_TEXT, shareOrderDetails)
+    sendIntent.type = "text/plain"
+
+    val shareIntent = Intent.createChooser(sendIntent, null)
+
+    if(context !is android.app.Activity){
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    }
+    context.startActivity(shareIntent)
 }
--- app/src/main/java/com/example/starsucksapp/OrderDetailsActivity.kt
+++ app/src/main/java/com/example/starsucksapp/OrderDetailsActivity.kt
@@ -5,16 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.starsucksapp.databinding.ActivityOrderDetailsBinding
 
 class OrderDetailsActivity : AppCompatActivity() {
+
+    var order = order()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_order_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        val binding = ActivityOrderDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //order.productName = intent.getIntExtra("order", -1).toString()
+        order.productName = intent.getStringExtra("order") ?: "Unknown Product"
+
+        binding.tvPlaceHolder.text = order.productName
+
+        when(order.productName){
+            "Soy Latte" -> binding.imageView3.setImageResource(R.drawable.sb1)
+            "Chocco Frappe" -> binding.imageView3.setImageResource(R.drawable.sb1)
+            "Bottled Americano" -> binding.imageView3.setImageResource(R.drawable.sb2)
+        }
+        binding.fabOrder.setOnClickListener {
+            shareIntent(applicationContext, order.productName)
+        }
+        /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
     }
 }
